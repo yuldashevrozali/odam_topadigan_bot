e967146cd01beb41ad8eb211665fcb22bb112010
Rozali <yuldashevrozali08@gmail.com>
2025-12-19 19:10:44 +0500

    aaaz

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ const BLACKLIST = [
   "pochta ham olamiz","почта ҳам оламиз",
   "srochni yuramiz","srochni ketamiz","srochniy yuramiz",
   "odam qo'shish","1 odam kerak","2 odam kerak","3 odam kerak","ODAM KERAK","Manzildan","AVTO",
-  "joyimiz bor","odam pochta","poshda olamiz","ONIX taksi","Taksi Bar","Adam Poshta Bolsa",
+  "joyimiz bor","odam pochta","poshda olamiz","ONIX taksi","Taksi Bar","Adam Poshta Bolsa","1 kishi kera","DAROMAD TOPISHNI XOHLAYSIZMI?","DAROMAD TOPISHNI XOHLAYSIZMI","POSHTA OLAMIZ","POSHTA OLAMAN","Taksi bor","Taksi bormi","Yuk bor","Yuk bormi",
   "1 odam garak","2 odam garak","3 odam garak","POCHTA HAM OLAMAN","jonaymiz","pochtaham olamiz","3 kishi  kerak","4 kishi kerak","5 kishi kerak",
   "srochni getamiz","moshin bor","pochta olomon","TAXI BOR","POCHTA HAM OLAMIZ","mashin bor","2 KISHI KERAK","TAXI BAR","1 KISHI KERE","pochta xam olamiz","POSHTAXAM OLAMIZ","2 KISHI KK","1 kishi kere","YURAMIZ","POCHTA OLAMIZ"
 ];
